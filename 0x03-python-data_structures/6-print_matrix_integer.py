9ead1dc6a7cc91ca5cf51abdf5f049a2c7f5dfc4
JosephJohncross <josephibochi@gmail.com>
2023-11-06 17:19:38 +0100

    g-print-matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for mat in matrix:
+        list_len = len(mat)
+        for int in mat:
+            if (mat.index(int) == list_len - 1):
+                print("{:d} ".format(int))
+            else:
+                print("{:d} ".format(int), end="")
+        print()
