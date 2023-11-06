4fecbab92d5301486724b2383a5222f97bd21159
JosephJohncross <josephibochi@gmail.com>
2023-11-06 17:04:13 +0100

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     new_str = ""
 
-    for char in my_string:
-        if char not in ["C", "c"]:
-            new_str += char
+    for j in my_string:
+        if j not in ["C", "c"]:
+            new_str += j
     return new_str
